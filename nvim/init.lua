99e5141bd7c3fde26ec104658bdef346bee79f46
Markus <m.gross@mac.com>
2023-07-28 06:52:50 +0200

    style

--- nvim/init.lua
+++ nvim/init.lua
@@ -25,16 +25,16 @@ require("user.telescope")
 require("user.tree-sitter")
 
 local has = function(x)
-  return vim.fn.has(x) == 1
+	return vim.fn.has(x) == 1
 end
 
 local is_mac = has("macunix")
 local is_win = has("win32")
 
 if is_mac then
-  require("user.macos")
+	require("user.macos")
 end
 
 if is_win then
-  require("user.windows")
+	require("user.windows")
 end
